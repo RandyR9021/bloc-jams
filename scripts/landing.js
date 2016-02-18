d078dcdc9ff1d7155f4c6c82fc07bc3a1dd8be80
Randyr902 <rdj7890@gmail.com>
2016-02-18 14:17:59 -0800

    assignment-23-dom-scripting completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,30 +1,25 @@
 var pointsArray = document.getElementByClassName('point');
 
-var animatePoints = function(points) {
-    }
-    
-var revealPoint = function(index) {
-    points[index].style.opacity = 1;
-    points[index].style.transform = "scaleX(1) translateY(0)";
-    points[index].style.msTransform = "scaleX(1) translateY(0)";
-    points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+var revealPoint = function(point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
                   
   };
-                 
-  for (var i = 0; i < points.length; i++) {
-    revealPoint(i);
-  }
+
+var animatePoints = function(points) {
+    forEach(points, revealPoint);
 };
 
 window.onload = function() {
-    // Aiutomatically animate he points on a tall screen where scrolling can't trigger the animation
+    // Automatically animate the points on a tall screen where scrolling can't trigger the animation
     if (window.innerHeight > 950) {
         animatePoints(pointsArray);
     }
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
     window.addEventListener('scroll', function(event) {
-        if (documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+        if (pointsArray[0].getBoundingClientRect().top <= 500) {
             animatePoints(pointsArray);
         }
     });
