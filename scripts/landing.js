34bc2a96f5e01a1d54db0d49a71b38f96c783232
Randyr902 <rdj7890@gmail.com>
2016-03-09 13:29:11 -0800

    checkpoint-29-jquery completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,26 +1,37 @@
-var pointsArray = document.getElementByClassName('point');
-
-var revealPoint = function(point) {
-    point.style.opacity = 1;
-    point.style.transform = "scaleX(1) translateY(0)";
-    point.style.msTransform = "scaleX(1) translateY(0)";
-    point.style.WebkitTransform = "scaleX(1) translateY(0)";
-                  
-  };
-
-var animatePoints = function(points) {
-    forEach(points, revealPoint);
-};
-
-window.onload = function() {
-    // Automatically animate the points on a tall screen where scrolling can't trigger the animation
-    if (window.innerHeight > 950) {
-        animatePoints(pointsArray);
+      //#5
+ var animatePoints = function() {
+            
+    var revealPoint = function() {
+     
+        //#7
+     $(this).css({
+        opacity: 1,
+        transform: 'scaleX(1) translateY(0)'
+});
+    };
+            
+    //#6
+    $.each($('.point'), revealPoint);
+  });
+    
+$(window).load(function) {
+    //#1
+    if ($(window).height() > 950) {
+        animatePoints();
     }
+    //#2
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+   
+    //#3
+    $(window).scroll(function(event) {
+        
+        //#4
+        if($(window).scrollTop() >= scrollDistance) {
+            animatePoints();
+        }
     
     window.addEventListener('scroll', function(event) {
         if (pointsArray[0].getBoundingClientRect().top <= 500) {
             animatePoints(pointsArray);
         }
     });
-}
