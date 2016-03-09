015af25f06d52c2690fb54a5089ea01987446343
Randyr902 <rdj7890@gmail.com>
2016-03-09 13:48:48 -0800

    cleaned up landing page

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,29 +1,17 @@
-      //#5
- var animatePoints = function() {
-            
-    var revealPoint = function() {
-     
-        //#7
-     $(this).css({
-        opacity: 1,
-        transform: 'scaleX(1) translateY(0)'
-});
-    };
-            
-    //#6
-    $.each($('.point'), revealPoint);
-  });
+    $(window).load(function) {
     
-$(window).load(function) {
-    //#1
+        //#1
     if ($(window).height() > 950) {
         animatePoints();
     }
-    //#2
+    
+        //#2
     var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
    
-    //#3
+    
+        //#3
     $(window).scroll(function(event) {
+        
         
         //#4
         if($(window).scrollTop() >= scrollDistance) {
@@ -35,3 +23,22 @@ $(window).load(function) {
             animatePoints(pointsArray);
         }
     });
+
+        //#5
+ var animatePoints = function() {
+            
+    var revealPoint = function() {
+     
+        //#7
+     $(this).css({
+        opacity: 1,
+        transform: 'scaleX(1) translateY(0)'
+});
+    
+    };
+            
+    
+     //#6
+    $.each($('.point'), revealPoint);
+  });
+    
